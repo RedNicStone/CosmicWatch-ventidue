bea928e7f7df9adf95072218e7d32f2fe3b673bc
RedNicStone <nic@struktur.de>
2022-09-05 10:27:19 +0200

    Updated demo to newer API version

--- software/arduino/tests/other/simple_demo.cpp
+++ software/arduino/tests/other/simple_demo.cpp
@@ -20,19 +20,22 @@ int main() {
     unsigned int height = 128;
     unsigned int width = 128;
 
-    PixelBuffer<RGB8> image(Point2D(height, width));
-    auto sdf = MVisualization(TTranslate(TScale(CInterpolate(
+    auto sdf = TTranslate(TScale(CInterpolate(
         GSegment(vec2(-35.0f), vec2(35.0f), 10.0f),
         TRotate(GSegment(vec2(-35.0f), vec2(35.0f), 10.0f), 90)
-        , 0.9), 1), vec2(64)));
+        , 0.9), 1), vec2(64));
+
+    auto pixel = MRGB8(MVisualization(sdf));
+    auto raster = SRasterizer<RGB8, decltype(pixel)>(pixel);
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    FileWriter::writeImageToFile("demo.bmp", raster, width, height);
+
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()
-    << "[us]" <<
-    std::endl;
-
-    FileWriter::writeImageToFile("demo.bmp", (const unsigned char *)(image.getDataHandle()), width, height);
+              << "[us]" <<
+              std::endl;
 
     system("feh demo.bmp --zoom 1000 --force-aliasing");
 }
